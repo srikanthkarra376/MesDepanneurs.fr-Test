0ec9773ab8ca293daa2e7845530c1728b4b698d6
Srikanth Karra <srikanthreddy376@gmail.com>
2019-02-10 15:04:26 +0100

    added

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "srikanth";
+$servername = "localhost";  #please change to your server name
+$username = "root";  #please change to your user name
+$password = "srikanth"; #please change to your password
 $dbname = "mesdepanneurs_db";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
